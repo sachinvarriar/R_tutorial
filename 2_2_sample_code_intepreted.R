dd9a99d1b2ca16c7a8ae0918ff73dece2ec555aa
Sachin Varriar <sachin.varriar@gmail.com>
2022-01-08 09:58:39 +0530

    updated repo with more sample demos of  Rstudio and Rmarkdown

---
+++ 2_2_sample_code_intepreted.R
@@ -0,0 +1,10 @@
+#this is a comment
+#assigning value 3 to a
+a<-3
+#printing a
+print(a)
+b<-2
+#performing addition of a and b, using the + operator and storing in c
+c<-a+b
+#printing c
+print(c)
